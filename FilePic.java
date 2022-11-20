8734cab260c3fb586944d7ce0e0088c3104d8cc7
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2022-11-20 11:48:24 +0530

    Picture added

---
+++ FilePic.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class FilePic extends Frame {
+    FilePic()
+    {
+        setVisible(true);
+        setSize(800,800);
+    }
+}
--- TicTacToe.java
+++ TicTacToe.java
@@ -1,16 +1,17 @@
 import java.awt.*;
 import java.awt.event.*;
+import javax.swing.*;
 
-public class TicTacToe extends Frame implements ActionListener {
+public class TicTacToe extends JFrame implements ActionListener {
     CardLayout cardLayout = new CardLayout(20, 20);
+    Font f = new Font("Comic Sans MS", Font.BOLD, 20);
     // Welcome screen
     Panel welcomPanel = new Panel();
     Label wel = new Label(" Welcome to Tic Tac Toe!! ");
     Button play = new Button("Play");
     Button exit = new Button("Exit");
-    Panel labPanel = new Panel();
-    Panel butPanel = new Panel();
-
+    Panel pic=new Panel();
+    JLabel imagelabel=new JLabel();
     // Selection screen which contains Grid Layout
     Panel home = new Panel();
     Label Name = new Label("Tic Tac Toe");
@@ -18,7 +19,7 @@ public class TicTacToe extends Frame implements ActionListener {
     Button back2 = new Button("Back");
 
     // Message Panel
-    Panel Mess = new Panel();
+    JPanel Mess = new JPanel();
     Label messLabel = new Label();
     Button confirm = new Button("Confirm");
     Button back1 = new Button("Back");
@@ -29,7 +30,7 @@ public class TicTacToe extends Frame implements ActionListener {
     Label O = new Label("O:");
 
     // Card Layout panel
-    Panel card = new Panel();
+    JPanel card = new JPanel();
 
     public void actionPerformed(ActionEvent a) {
         if (a.getSource() == play) {
@@ -48,17 +49,18 @@ public class TicTacToe extends Frame implements ActionListener {
 
     TicTacToe() {
         setVisible(true);
-        setSize(500, 500);
+        setSize(800, 800);
         add(welcomPanel);
         welcomPanel.setBackground(Color.ORANGE);
-        welcomPanel.setLayout(new BorderLayout());
-        labPanel.add(wel);
-        welcomPanel.add(labPanel, BorderLayout.CENTER);
-        butPanel.setLayout(new FlowLayout());
-        butPanel.add(play);
-        butPanel.add(exit);
-        welcomPanel.add(butPanel, BorderLayout.SOUTH);
-
+        welcomPanel.setLayout(null);
+        wel.setBounds(250, 10, 250, 50);
+        wel.setFont(f);
+        setForeground(Color.BLUE);
+        welcomPanel.add(wel);
+        imagelabel.setBounds(80, 90, 628, 611);
+        imagelabel.setIcon(new ImageIcon("D:\\Documents\\DDU\\DDU Sem 3\\JAVA\\TicTacToe\\tictactoe.jpg"));
+        // pic.add(imagelabel);
+        welcomPanel.add(imagelabel);
         // setting card layout
         card.setLayout(cardLayout);
 
